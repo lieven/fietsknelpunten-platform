4ee3acce90b15d386af8e8f8cbe8d610049938ff
Klaas Cuvelier <cuvelierklaas@gmail.com>
2015-02-02 23:38:04 +0100

    Update issues API route

--- app/controllers/api/v1/IssuesController.php
+++ app/controllers/api/v1/IssuesController.php
@@ -5,7 +5,12 @@ class IssuesController extends BaseController
 
 	public function listAction()
 	{
+		$issues = array(
+			array('id' => 1),
+			array('id' => 2)
+		);
 
+		return Response::json( $issues);
 	}
 
 	public function createAction()
