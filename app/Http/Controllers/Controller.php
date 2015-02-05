48613f0c187a89bc5c535aabb01f8c935fe9a46a
Klaas Cuvelier <cuvelierklaas@gmail.com>
2015-02-05 19:25:28 +0100

    Add general methods for paging (input and output)

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,4 +8,38 @@ abstract class Controller extends BaseController {
 
 	use DispatchesCommands, ValidatesRequests;
 
+
+	protected function getPaging()
+	{
+		return array(
+			'offset' 	=> 0,
+			'limit'		=> 10
+		);
+	}
+
+	/**
+	 * @param int $offset
+	 * @param int $limit
+	 * @param int $total
+	 * @param array $data
+	 * @param string $routeName
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	protected function generateResponse($offset, $limit, $total, $data, $routeName)
+	{
+		$previousOffset = $offset - $limit > 0 ? $offset - $limit : 0;
+		$nextOffset		= $offset + $limit;
+
+		return response()->json(
+			array(
+				'count'		=> count($data),
+				'total'		=> $total,
+				'data' 		=> $data,
+				'paging'	=> array(
+					'previous'	=> route($routeName) . '?offset=' . $previousOffset . '&limit=' . $limit,
+					'next'		=> route($routeName) . '?offset=' . $nextOffset . '&limit=' . $limit
+				)
+			)
+		);
+	}
 }
