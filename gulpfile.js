8e751337f028ed731d5a4b54d43cd6dfe13e7cf8
Klaas Cuvelier <klaascuvelier@Klaass-iMac.local>
2015-03-16 22:09:33 +0100

    Add gulp + npm stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
-var elixir = require('laravel-elixir');
+'use strict';
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
 
-elixir(function(mix) {
-    mix.less('app.less');
-});
+var del         = require('del');
+var path        = require('path');
+var gulp        = require('gulp');
+var cookbook    = require('gulp-cookbook');
+
+
+config.paths = {};
+config.paths.dist       = 'public';
+config.paths.source     = 'resources';
+config.paths.components = path.join(config.paths.source, 'components');
+
+config.paths.jsSource   = [
+    path.join(config.paths.components, 'app/src/**/*.module.js'),
+    path.join(config.paths.components, '**/src/**/*.module.js'),
+    path.join(config.paths.components, '**/src/**/*.config.js'),
+    path.join(config.paths.components, '**/src/**/*.js')
+];
+
+config.paths.lessSource = path.join(config.paths.components, '**/src/**/*.less');
+
+gulp.task('scripts', ['clean-build'], cookbook.building.scripts(config.paths.jsSource, config.paths.dist, { concat: 'app.js', sourceMaps: config.paths.dist }));
+gulp.task('styles', ['clean-build'], cookbook.building.less(config.paths.lessSource, config.paths.dist, { concat: 'app.css', sourceMaps: config.paths.dist }));
