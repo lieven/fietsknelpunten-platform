f63c348f311a88ab85e672e8f02bb065c25c3cb9
Lieven Dekeyser <lievendekeyser@gmail.com>
2016-11-12 22:37:31 +0100

    API: tags

--- site/apimodule.php
+++ site/apimodule.php
@@ -4,7 +4,8 @@ class ApiModule extends AbstractApiModule
 {
 	private static $parameters = array
 	(
-		'list' => array()
+		'list' => array(),
+		'tags' => array()
 	);
 	
 	function __construct()
@@ -37,5 +38,61 @@ class ApiModule extends AbstractApiModule
 		);
 	
 		$this->outputJson($list);
+	}
+	
+	// temporary function until tags are available in database
+	private function tags()
+	{
+		$groupID = 1;
+		$tagID = 1;
+		
+		$group = function($name, $tagsArray) use(&$groupID)
+		{
+			return array
+			(
+				'id' => $groupID++,
+				'name' => $name,
+				'tags' => $tagsArray
+			);
+		};
+		
+		$tag = function($name, $info) use(&$tagID)
+		{
+			return array
+			(
+				'id' => $tagID++,
+				'name' => $name,
+				'info' => $info
+			);
+		};
+		
+		$results = array
+		(
+			$group('Staat van het wegdek', array(
+				$tag('Put', 'Een put, ophoping, verzakking of scheur in het wegdek verhoogt het risico op vallen'),
+				$tag('Afwatering', 'Bij regenweer is er een slechte afwatering met vorming van plassen op het fietspad'),
+				$tag('Gevaarlijke rand', 'Overgangen met niveauverschil (bv. boordstenen) zijn te hoog'),
+				$tag('Begroeiing', 'Bermbegroeiing woekert en versmalt het fietspad'),
+				$tag('Glas', 'Er ligt glas op het fietspad met gevaar voor lekrijden'),
+				$tag('Wegdek: andere', 'Te specifiëren in beschrijving')
+			)),
+			$group('Signalisatie', array( 
+				$tag('Fietspad', 'Wegmarkeringen die het fietspad aanduiden ontbreken of zijn slecht zichtbaar'),
+				$tag('Oversteekplaats', 'Wegmarkeringen die een fietsoversteekplaats aanduiden zijn slecht zichtbaar'),
+				$tag('Verkeersborden', 'Verkeersborden die het fietspad aanduiden ontbreken of zijn in slechte staat'),
+				$tag('Verkeerslicht', 'Een verkeerslicht is defect of niet correct afgesteld'),
+				$tag('Onduidelijk', 'De gebruikte signalisatie is niet reglementair of de voorrangssituatie is onduidelijk'),
+				$tag('Signalisatie: andere')
+			)),
+		);
+		
+		return $results;
+	}
+	
+	
+	function tagsAction()
+	{
+		$results = $this->tags();
+		$this->outputJson($results);
 	}
 }
