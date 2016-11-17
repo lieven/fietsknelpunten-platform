69e4ab1af6d0d55c35ae9b5cc0ee5c98162a895d
Lieven Dekeyser <lievendekeyser@gmail.com>
2016-11-17 11:38:02 +0100

    Improve config access

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
-define('SITE_FOLDER', 'src');
+define('CONFIG_FILE', '../fietsknelpunten-platform-config.php');
 
+define('SITE_FOLDER', 'src');
 set_include_path(SITE_FOLDER);
 
 require_once('system/run.php');
