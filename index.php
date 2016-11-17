56c4b0c83cc8feff3b913f1a7bb78025d35992b1
Lieven Dekeyser <lievendekeyser@gmail.com>
2016-11-17 12:06:29 +0100

    Update for simpler autoloader

--- index.php
+++ index.php
@@ -2,7 +2,5 @@
 
 define('CONFIG_FILE', '../fietsknelpunten-platform-config.php');
 
-define('SITE_FOLDER', 'src');
-set_include_path(SITE_FOLDER);
 
-require_once('system/run.php');
+require_once('src/Base/run.php');
